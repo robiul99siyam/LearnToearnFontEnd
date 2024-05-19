2265863976cf3aaf990a4ec53971320f15d2a499
robiul99siyam <siyamrobiulislam@gmail.com>
2024-05-19 15:01:49 +0600

    footer and contact page create here

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './data/Index'; 
+import Contact from './pages/Contact';
 
 function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                {/* Add more routes here if needed */}
-            </Routes>
-        </BrowserRouter>
-    );
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/contact' element={<Contact />} />
+
+				{/* Add more routes here if needed */}
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
--- src/data/Index.jsx
+++ src/data/Index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from '../pages/Navbar';  
 import Hero from './../pages/Hero';
+import Footer from './../pages/Footer';
 
 const Home = () => {
     return (
         <>
             <Navbar />
             <Hero />
+            <Footer />
         </>
     );
 };
